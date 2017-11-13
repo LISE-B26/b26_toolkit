017d71d1d6d8f0f97bda53e7a4dd22a58a5952a0
Aaron <akabcenell@g.harvard.edu>
2017-11-13 00:42:12 -0500

    Rewrote dcservo_kinesis_dll to also support KCubes in addition to TCubes

--- src/instruments/__init__.py
+++ src/instruments/__init__.py
@@ -33,6 +33,7 @@ from labview_fpga import NI7845RMain
 from newport_smc100 import SMC100
 from awg import AWG
 from keysight_oscilloscope import Oscilloscope
+from dcservo_kinesis_dll import KDC001
 
 # from labview_fpga import NI7845RReadWrite, NI7845RPidSimpleLoop, FPGA_GalvoScan, NI7845RReadFifo
 
